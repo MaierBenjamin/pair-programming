f4083587b9034e0d583dcc86ccc8b8548ed21011
Benjamin Maier <benjamin_maier@sluz.ch>
2025-04-04 15:15:30 +0200

    Aufgabe0: erledigt, isLeapYear Funktion mit else if korrigiert

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,13 @@
 export function isLeapYear(year: number): boolean {
   // FIXME: this implementation is wrong!
-  return year != 0 && Math.random() > 0.5;
+if (year %100 == 0 && year %400 !=0) {
+  return false;
+} 
+else if ( year %4 == 0)
+{
+  return true;
+}
+
+else {return false;}
+
 }
