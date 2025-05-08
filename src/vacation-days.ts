fb669fb3fbbf3089d9fbc97f3d30d65ef569aca3
Pascal <pascal_buehler@sluz.ch>
2025-05-08 21:52:12 +0200

    Aufgabe1, Testcase erstellt mit 40% und volles Jahr. Funktion für leapYear erstellt und auch die Funktion für die Anzahl der Tage in einem Monat.

--- src/vacation-days.ts
+++ src/vacation-days.ts
@@ -7,5 +7,22 @@ export type Employment = {
 
 export function calculateProRataVacationDays(employment: Employment): number {
   // TODO: calculate pro rata (consider workload and days worked)
+  function isLeapYear(year: number): boolean {
+    // FIXME: this implementation is wrong!
+  if (year %100 == 0 && year %400 !=0) {
+    return false;
+  } 
+  else if ( year %4 == 0)
+  {
+    return true;
+  }
+}
+function getDaysInMonth(year: number, month: number): number {
+    // Handle month index (0-11) and year
+    const date = new Date(year, month + 1, 0); // Get the last day of the month
+    return date.getDate();
+  }
+  
   return employment.vacationDays;
 }
+
